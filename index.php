997db8977df0e327b768bc680cdbfcdb69aab62b
PhanQuocDat-2102 <datphan2102@gmail.com>
2021-12-01 16:30:43 +0700

    update

--- index.php
+++ index.php
@@ -35,12 +35,12 @@
                     </div>
                     <div class="header__right-button">
 
-                    <?php if(isset($_SESSION['taiKhoan'])){?>
+                    <?php if(isset($_SESSION['emailAddress'])){?>
                             <script>alert('Đăng nhập thành công');</script>
-                            <a> Chào, <?php echo $_SESSION['taiKhoan'];?></a>
+                            <a> Chào, <?php echo $_SESSION['emailAddress'];?></a>
                             <button class="header__btn btn" >
-                                <a href="./login.php" class = "header__btn-link"  style = "font-size: 1.4rem;  text-decoration: none; color: var(--white-color);"  >
-                                 Đăng xuất <?php  unset($_SESSION['taiKhoan']);?>
+                                <a href="./index.php" class = "header__btn-link"  style = "font-size: 1.4rem;  text-decoration: none; color: var(--white-color);"  >
+                                 Đăng xuất <?php  unset($_SESSION['emailAddress']);?>
                                 </a></button>
                         <?php }else{ ?>
                             <button class="header__btn btn">Đăng ký</button>
@@ -50,8 +50,8 @@
                                 </a></button>
                         <?php } ?>
 
-                        <button class="header__btn btn">Đăng ký</button>
-                        <button class="header__btn btn">Đăng nhập</button>
+                        <!-- <button class="header__btn btn">Đăng ký</button>
+                        <button class="header__btn btn">Đăng nhập</button> -->
 
                     </div>
                 </div>
